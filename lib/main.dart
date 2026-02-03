b3af2fd557cc8b16c1d5c844c219b77820482a7e
donovantran <ddt004@ucsd.edu>
2026-02-02 16:35:17 -0800

    added the pages and widget directory to clean up main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           ],
         ),
         drawer: Drawer(
-          child: Text('Yo'),
+          child: Text('This works'),
         ),
       ),
     );
