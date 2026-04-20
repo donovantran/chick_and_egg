6d1a4b1c08e939a5455509ea8e5e42200c7dd9a0
donovantran <ddt004@ucsd.edu>
2026-04-20 13:55:43 -0700

    added a notes page

---
+++ lib/pages/notes_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../widgets/app_drawer.dart';
+import '../widgets/app_top_bar.dart';
+
+class NotesPage extends StatelessWidget {
+  const NotesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF835545),
+      appBar: const AppTopBar(title: 'Notes'),
+      drawer: const AppDrawer(),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/chick.png', // change to your real path
+              width: 180,
+              fit: BoxFit.contain,
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'notes pages',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
