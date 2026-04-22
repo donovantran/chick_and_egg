71a34e5c494f0a0b9674eddee9aa769f3940a6eb
donovantran <ddt004@ucsd.edu>
2026-04-22 15:39:51 -0700

    added an app shell to navigate

---
+++ lib/pages/app_shell.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+import '../widgets/app_drawer.dart';
+import '../widgets/app_top_bar.dart';
+import 'notes_page.dart';
+import 'settings_page.dart';
+import 'table_of_contents_page.dart';
+
+class AppShell extends StatefulWidget {
+  const AppShell({super.key, this.startIndex = 0});
+  final int startIndex;
+
+  @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  late int currentIndex;
+
+  final pages = const [
+    TableOfContentsPage(),
+    NotesPage(),
+    SettingsPage(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = widget.startIndex;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF835545),
+      //appBar: const AppTopBar(title: 'Chick and Egg'),
+      drawer: const AppDrawer(),
+      body: pages[currentIndex],
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: GNav(
+          selectedIndex: currentIndex,
+          onTabChange: (i) => setState(() => currentIndex = i),
+          gap: 8,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          tabBorderRadius: 16,
+          color: Colors.white70,
+          activeColor: Colors.white,
+          tabBackgroundColor: Colors.white.withOpacity(0.15),
+          tabs: const [
+            GButton(icon: Icons.menu_book, text: 'TOC'),
+            GButton(icon: Icons.note, text: 'Notes'),
+            GButton(icon: Icons.settings, text: 'Settings'),
+          ],
+        ),
+      ),
+    );
+  }
+}
