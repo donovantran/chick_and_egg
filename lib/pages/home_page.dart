0977c0e1be36d779480c92971f8fc813a085ce74
donovantran <ddt004@ucsd.edu>
2026-06-07 18:27:55 -0700

    removed the drawer and app top bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,26 +7,17 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF835545),
-      appBar: const AppTopBar(title: 'Home'),
-      drawer: const AppDrawer(),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/chick.png', // change to your real path
-              width: 180,
-              fit: BoxFit.contain,
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'home page',
-              style: TextStyle(color: Colors.white),
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/chick.png',width:180,fit: BoxFit.contain,),
+          const SizedBox(height: 16),
+          const Text(
+            'home page',
+            style: TextStyle(color: Colors.white)
+          ),
+        ],
       ),
     );
   }
