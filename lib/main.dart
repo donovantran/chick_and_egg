41d9def9ca53656ab7e39c569f0852d2a613cd3c
donovantran <ddt004@ucsd.edu>
2026-06-19 18:57:59 -0700

    got rid of the routing within main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets/app_drawer.dart';
-import 'widgets/app_top_bar.dart';
 import 'pages/app_shell.dart';
-import 'pages/settings_page.dart';
-import 'pages/table_of_contents_page.dart';
-import 'pages/notes_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,29 +9,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-
-      initialRoute: '/',
-
-      routes: {
-
-        '/': (_) => const AppShell(startIndex: 0),       // TOC tab
-
-        '/toc': (_) => const AppShell(startIndex: 0),
-
-        '/notes': (_) => const AppShell(startIndex: 1),
-
-        '/settings': (_) => const AppShell(startIndex: 2),
-
-      },
-
+      home: AppShell(),
     );
-
   }
-
 }
