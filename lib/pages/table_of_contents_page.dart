9083fa9acee77031ee94a0575473564d29c8bbc7
donovantran <ddt004@ucsd.edu>
2026-06-19 19:18:45 -0700

    removed the app drawer at the top

--- lib/pages/table_of_contents_page.dart
+++ lib/pages/table_of_contents_page.dart
@@ -7,26 +7,14 @@ class TableOfContentsPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF835545),
-      appBar: const AppTopBar(title: 'Table of Contents'),
-      drawer: const AppDrawer(),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/chick.png', // change to your real path
-              width: 180,
-              fit: BoxFit.contain,
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'table of contents',
-              style: TextStyle(color: Colors.white),
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/chick.png', width: 180, fit: BoxFit.contain,),
+          const SizedBox(height: 16,),
+          const Text('table of contents', style: TextStyle(color: Colors.white)),
+        ],
       ),
     );
   }
